d153d592d5225812e6a8dce7c30103de2ad2741a
Manuel Espejo <manuel.a.espejo@gmail.com>
2020-09-29 20:17:15 +0200

    Entrega Modulo 12 Banca online

--- src/pages/transfer/transfer.mappers.js
+++ src/pages/transfer/transfer.mappers.js
@@ -1,8 +1,9 @@
 export const mapTransferFromVmToApi = (transfer) => {
-  const transferDate = new Date();
-  transferDate.setFullYear(transfer.year);
-  transferDate.setMonth(transfer.month - 1);
-  transferDate.setDate(transfer.day);
+  const transferDate = new Date(
+    transfer.year + '-' + transfer.month + '-' + transfer.day
+  ).toLocaleDateString();
+  console.log({ transferDate });
+
   return {
     accountId: transfer.accountId,
     iban: transfer.iban,
@@ -11,14 +12,6 @@ export const mapTransferFromVmToApi = (transfer) => {
     concept: transfer.concept,
     notes: transfer.notes,
     date: transferDate,
-    // date:
-    //   transfer.year +
-    //   '-' +
-    //   (transfer.month <= 9 ? '0' : '') +
-    //   transfer.month +
-    //   '-' +
-    //   (transfer.day <= 9 ? '0' : '') +
-    //   transfer.day,
     email: transfer.email,
   };
 };
